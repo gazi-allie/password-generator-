902402df5164a7790d7aec79e4df60b00351973b
gazi-allie <ibnibilal114@gmail.com>
2024-03-02 21:43:43 +0530

    the password

---
+++ script.js
@@ -0,0 +1,14 @@
+const inputSlider= document.querySelector("[data-lengthslider]")
+const lengthDisplay=document.querySelector("[data-lengthNumber]");
+const passwordDisplay =document.querySelector("data-passwordDisplay");
+const copybtn = document.querySelector("copyBtn");
+const copyMsg =document.querySelector("[data-copyamsg]");
+const uppercase=document.querySelector("#uppercase");
+const lowerCase=document.querySelector("#lowercase");
+const numberChk=document.querySelector("#numbers");
+const symbolChk=document.querySelector("#symbols");
+const indicator=document.querySelector("[data-indicator]");
+const genrateBtn=document.querySelector("generatepassword");
+const allCheckBox=document.querySelector("input[type=checkbox]");
+
+
